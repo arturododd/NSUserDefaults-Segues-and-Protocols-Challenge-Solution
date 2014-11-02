ea8aa0b2353bc629d8044307f6a26029d4180292
Arturo Dodd <arturo@dodd.mx>
2014-11-02 16:57:09 -0600

    Initial Commit

---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/ADViewController.h
@@ -0,0 +1,13 @@
+//
+//  ADViewController.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Arturo Dodd Vaudrecourt on 11/2/14.
+//  Copyright (c) 2014 Dodd.mx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADViewController : UIViewController
+
+@end
---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/ADViewController.m
@@ -0,0 +1,29 @@
+//
+//  ADViewController.m
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Arturo Dodd Vaudrecourt on 11/2/14.
+//  Copyright (c) 2014 Dodd.mx. All rights reserved.
+//
+
+#import "ADViewController.h"
+
+@interface ADViewController ()
+
+@end
+
+@implementation ADViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
